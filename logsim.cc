b4d9f05dbdb82cf1e9d500922e37c438dba0b0e5
Oliver  Lambert <ocl20@cam.ac.uk>
2011-05-30 18:20:37 +0100

    Added functionality to logsim.cc to allow output to console under windows.  guicon.cpp and guicon.h are also required for this
    Signed-off-by: Oliver  Lambert <ocl20@cam.ac.uk>

--- logsim.cc
+++ logsim.cc
@@ -3,6 +3,10 @@
 #include "gui.h"
 #include <GL/glut.h>
 
+#if defined(_WINDOWS) && defined(_DEBUG)
+	#include "guicon.h"	
+#endif
+
 // #define USE_GUI
 
 IMPLEMENT_APP(MyApp)
@@ -10,7 +14,11 @@ IMPLEMENT_APP(MyApp)
 bool MyApp::OnInit()
   // This function is automatically called when the application starts
 {
-  if (argc != 2) { // check we have one command line argument
+	#if defined(_WINDOWS) && defined(_DEBUG)
+		RedirectIOToConsole();					// Required for cout in windows
+	#endif
+
+	if (argc != 2) { // check we have one command line argument
     wcout << "Usage:      " << argv[0] << " [filename]" << endl;
     exit(1);
   }
@@ -40,3 +48,67 @@ bool MyApp::OnInit()
   }
   return(false); // exit the application
 }
+
+
+//#include <windows.h>
+//#include <iostream>
+//#include <fstream>
+//#include <conio.h>
+//#include <stdio.h>
+//
+//#ifndef _USE_OLD_OSTREAMS
+//	using namespace std;
+//#endif
+//
+//#include "guicon.h"
+//#include <crtdbg.h>
+//
+//int APIENTRY WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,LPTSTR lpCmdLine,int nCmdShow)
+//{
+//	#ifdef _DEBUG
+//		RedirectIOToConsole();
+//	#endif
+//	
+//	int iVar;
+//	
+//	// test stdio
+//	fprintf(stdout, "Test output to stdout\n");
+//	fprintf(stderr, "Test output to stderr\n");
+//	fprintf(stdout, "Enter an integer to test stdin: ");
+//	scanf("%d", &iVar);
+//	printf("You entered %d\n", iVar);
+//
+//	//test iostreams
+//	cout << "Test output to cout" << endl;
+//	cerr << "Test output to cerr" << endl;
+//	clog << "Test output to clog" << endl;
+//	cout << "Enter an integer to test cin: ";
+//	cin >> iVar;
+//	cout << "You entered " << iVar << endl;
+//
+//	#ifndef _USE_OLD_IOSTREAMS
+//		// test wide iostreams
+//		wcout << L"Test output to wcout" << endl;
+//		wcerr << L"Test output to wcerr" << endl;
+//		wclog << L"Test output to wclog" << endl;
+//		wcout << L"Enter an integer to test wcin: ";
+//		wcin >> iVar;
+//		wcout << L"You entered " << iVar << endl;
+//	#endif
+//
+//	// test CrtDbg output
+//	_CrtSetReportMode( _CRT_ASSERT, _CRTDBG_MODE_FILE );
+//	_CrtSetReportFile( _CRT_ASSERT, _CRTDBG_FILE_STDERR );
+//	_CrtSetReportMode( _CRT_ERROR, _CRTDBG_MODE_FILE );
+//	_CrtSetReportFile( _CRT_ERROR, _CRTDBG_FILE_STDERR);
+//	_CrtSetReportMode( _CRT_WARN, _CRTDBG_MODE_FILE );
+//	_CrtSetReportFile( _CRT_WARN, _CRTDBG_FILE_STDERR);
+//	_RPT0(_CRT_WARN, "This is testing _CRT_WARN output\n");
+//	_RPT0(_CRT_ERROR, "This is testing _CRT_ERROR output\n");
+//	_ASSERT( 0 && "testing _ASSERT" );
+//	_ASSERTE( 0 && "testing _ASSERTE" );
+//	
+//	Sleep(2000);
+//	return 0;
+//}
+////End of File
