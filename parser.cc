ea0dfaf1fa43f1f64f700e211c4d44cea27c0719
Andrew Marshall <ajm268@cam.ac.uk>
2011-06-01 13:00:01 +0100

    Improved error reporting from parser.cc
    Changes:
     -made catches call print_err() when appropriate
     -made catches respond more intelligently so multiple exceptions
      are not raised for the same error
     -added errcount to track total errors in a def file
     -fixed bug in parser::section() so program does not hang if final
      '}' missing. Still need to make it spot the error though
    
    Not yet done:
     -semantic error checking

--- parser.cc
+++ parser.cc
@@ -8,15 +8,22 @@ using namespace std;
 
 bool parser::readin (void)
 {
-  try {
+//  try {
     parsedeffile();
-    cout << "It worked!" << endl;
-    return true;
-  }
-  catch (exception& e) {
-    cout << "Failed to correctly parse definition file." << endl;
-    return false;
-  }
+//    cout << "It worked!" << endl;
+	if (errcount == 0) {
+		cout << "Definition file parsed successfully" << endl;
+		return true;
+	}
+	else {
+		cout << "Could not parse definition file: " << errcount << " errors found" << endl;
+		return false;
+	}
+//  }
+//  catch (exception& e) {
+//    cout << "Failed to correctly parse definition file." << endl;
+//    return false;
+//  }
 }
 
 parser::parser (/*network* network_mod, devices* devices_mod,
@@ -31,14 +38,26 @@ parser::parser (/*network* network_mod, devices* devices_mod,
 
  // cursym = *new symbol();
 	symbol cursym;
+	errcount = 0;
 
 }
 
+class parseex : public exception // thrown by catch statements to indicate failed
+				                 // parsing once error has been reported
+{
+	public:
+		virtual const char* what () const throw ()
+		{
+			return "Exception: failed to parse definition file";
+		}
+} parseex_i;
+
 class uintex : public exception
 {
+	public:
   virtual const char* what () const throw ()
   {
-    return "Exception: invalid uint in definition file";
+    return "Exception: expected a uint";
   }
 } uintex_i;
 
@@ -46,29 +65,31 @@ void parser::uint (void) // throws uintex if not given a valid uint
 {
 try
   {
-    if (cursym.get_type() != Uint) throw uintex_i; // didn't get uint
+    if (cursym.get_type() != Uint) {errcount++; throw uintex_i;} // didn't get uint
   }
 catch(exception& e)
   {
-    cout << e.what() << endl;
+    //cout << e.what() << endl;
+	smz->print_err(e.what());
     throw;
   }
 }
 
-class boolruleex : public exception
-{
-  virtual const char* what () const throw ()
-  {
-    return "Exception: invalid bool in definition file";
-  }
-} boolruleex_i;
+//class boolruleex : public exception
+//{
+//  virtual const char* what () const throw ()
+//  {
+//    return "Exception: invalid bool in definition file";
+//  }
+//} boolruleex_i;
 
 
 class innameex : public exception
 {
+	public:
   virtual const char* what () const throw ()
   {
-    return "Exception: invalid inname in definition file";
+    return "Exception: expected an inname";
   }
 } innameex_i;
 
@@ -76,20 +97,22 @@ void parser::inname (void) // throws innameex if not given a valid inname
 {
 try
   {
-    if (cursym.get_type() != Inname) throw innameex_i; // didn't get an inname
+    if (cursym.get_type() != Inname) {errcount++; throw innameex_i;} // didn't get an inname
   }
  catch (exception& e)
    {
-     cout << e.what() << endl;
+//    // cout << e.what() << endl;
+//	smz->print_err(e.what());
      throw;
    }
 }
 
 class outnameex : public exception
 {
+	public:
   virtual const char* what () const throw ()
   {
-    return "Exception: invalid outname in definition file";
+    return "Exception: expected an outname";
   }
 } outnameex_i;
 
@@ -97,20 +120,22 @@ void parser::outname (void) // throws outnameex if not given a valid outname
 {
 try
   {
-    if (cursym.get_type() != Outname) throw outnameex_i; // didn't get an outname
+    if (cursym.get_type() != Outname) {errcount++; throw outnameex_i;} // didn't get an outname
   }
  catch (exception& e)
    {
-     cout << e.what() << endl;
+//   //  cout << e.what() << endl;
+//	smz->print_err(e.what());
      throw;
    }
 }
 
 class deviceex : public exception
 {
+	public:
   virtual const char* what () const throw ()
   {
-    return "Exception: invalid device in definition file";
+    return "Exception: expected a device";
   }
 } deviceex_i;
 
@@ -131,9 +156,9 @@ try
 	      smz->getsymbol (cursym);
 	      if (cursym.get_type() == Uint) { // i.e. symbol is a uint
 		return; // we have a valid CLOCK
-	      } else throw deviceex_i; // expected a uint
-	    } else throw deviceex_i; // expected 'period'
-	  } else throw deviceex_i; // expected '-'
+	      } else {errcount++; throw deviceex_i;} // expected a uint
+	    } else {errcount++; throw deviceex_i;} // expected 'period'
+	  } else {errcount++; throw deviceex_i;} // expected '-'
 	}
 
 	if (cursym.get_value() == SWITCH) { //i.e. devname is SWITCH
@@ -145,9 +170,9 @@ try
 	      if (cursym.get_type() == Uint &&
 		  (cursym.get_uint() == 0 || cursym.get_uint() == 1)) { // i.e. symbol is a uint but is also a bool
 		return; // we have a valid SWITCH
-	      } else throw deviceex_i; // expected a bl
-	    } else throw deviceex_i; // expected '-initialvalue'
-      	  } else throw deviceex_i; // expected '-'
+	      } else {errcount++; throw deviceex_i;} // expected a bl
+	    } else {errcount++; throw deviceex_i;}// expected '-initialvalue'
+      	  } else {errcount++; throw deviceex_i;} // expected '-'
 	}
 
 	if (cursym.get_value() == AND) { //i.e. devname is AND
@@ -158,9 +183,10 @@ try
 	      smz->getsymbol (cursym);
 	      if (cursym.get_type() == Uint) { // i.e. symbol is a uint
 		return; // we have a valid AND
-	      } else throw deviceex_i; // expected a uint
-	    } else throw deviceex_i; // expected '-numinputs'
-      	  } else throw deviceex_i; // expected '-'
+	      } else {errcount++; throw deviceex_i;} // expected a uint
+	    } else {errcount++; throw deviceex_i;} // expected 'numinputs'
+	  } else {errcount++; throw deviceex_i;} // expected '-'
+
 	}
 
 	if (cursym.get_value() == NAND) { //i.e. devname is NAND
@@ -171,9 +197,9 @@ try
 	      smz->getsymbol (cursym);
 	      if (cursym.get_type() == Uint) { // i.e. symbol is a uint
 		return; // we have a valid NAND
-	      } else throw deviceex_i; // expected a uint
-	    } else throw deviceex_i; // expected '-numinputs'
-      	  } else throw deviceex_i; // expected '-'
+	      } else {errcount++; throw deviceex_i;} // expected a uint
+	    } else {errcount++; throw deviceex_i;} // expected 'numinputs'
+	  } else {errcount++; throw deviceex_i;} // expected '-'
 	}
 
 	if (cursym.get_value() == OR) { //i.e. devname is OR
@@ -184,9 +210,9 @@ try
 	      smz->getsymbol (cursym);
 	      if (cursym.get_type() == Uint) { // i.e. symbol is a uint
 		return; // we have a valid OR
-	      } else throw deviceex_i; // expected a uint
-	    } else throw deviceex_i; // expected '-numinputs'
-      	  } else throw deviceex_i; // expected '-'
+	      } else {errcount++; throw deviceex_i;} // expected a uint
+	    } else {errcount++; throw deviceex_i;} // expected 'numinputs'
+	  } else {errcount++; throw deviceex_i;} // expected '-'
 	}
 
 	if (cursym.get_value() == NOR) { //i.e. devname is NOR
@@ -197,9 +223,9 @@ try
 	      smz->getsymbol (cursym);
 	      if (cursym.get_type() == Uint) { // i.e. symbol is a uint
 		return; // we have a valid NOR
-	      } else throw deviceex_i; // expected a uint
-	    } else throw deviceex_i; // expected '-numinputs'
-      	  } else throw deviceex_i; // expected '-'
+	      } else {errcount++; throw deviceex_i;} // expected a uint
+	    } else {errcount++; throw deviceex_i;} // expected 'numinputs'
+	  } else {errcount++; throw deviceex_i;} // expected '-'
 	}
 
 	if (cursym.get_value() == DTYPE) { //i.e. devname is DTYPE
@@ -211,21 +237,22 @@ try
 	}
 
 	throw deviceex_i; // didn't get a valid devname
-    } else throw deviceex_i; // didn't get a devname to start with
+    } else {errcount++; throw deviceex_i;} // didn't get a devname to start with
   }
  catch (exception& e)
    {
-     //cout << e.what() << endl;
-     smz->print_err(e.what());
+//     //cout << e.what() << endl;
+//     smz->print_err(e.what());
      throw;
    }
 }
 
 class unameex : public exception
 {
+	public:
   virtual const char* what () const throw ()
   {
-    return "Exception: invalid uname in definition file";
+    return "Exception: expected a uname";
   }
 } unameex_i;
 
@@ -234,17 +261,19 @@ void parser::uname (void) // throws unameex if not given a valid uname
 try
   {
 //    cout<<"running uname"<<endl;
-    if (cursym.get_type() != Uname) throw unameex_i; // didn't get a uname
+    if (cursym.get_type() != Uname) {errcount++; throw unameex_i;} // didn't get a uname
   }
  catch (exception& e)
    {
-     cout << e.what() << endl;
+//     //cout << e.what() << endl;
+//	smz->print_err(e.what());
      throw;
    }
 }
 
 class monruleex : public exception
 {
+	public:
   virtual const char* what () const throw ()
   {
     return "Exception: invalid monrule in definition file";
@@ -255,7 +284,7 @@ void parser::monrule (void) // throws monruleex if finds invalid monrule
 {
 try
   {
-    cout<<"testing monrule"<<endl;
+    //cout<<"testing monrule"<<endl;
     uname();
     smz->getsymbol (cursym);
     if (cursym.get_value() == dot) { // cursym is '.'
@@ -265,20 +294,23 @@ try
     } 
 	if (cursym.get_value() == semicolon) { // cursym is ';'
       return;
-    } else throw monruleex_i; // didn't get a '.' or ';'
+    } else {errcount++; throw monruleex_i;} // didn't get a '.' or ';'
   }
- catch (exception& e)
+ catch (exception& e) // Only catches an error in monrule syntax. Does not
+                      // throw it further, so subsequent lines can contine
+					  // to be parsed
    {
-     cout << e.what() << endl;
+	smz->print_err(e.what());
      while (!(cursym.get_value() == semicolon)) { // iterates while cursym is not ';'
        smz->getsymbol (cursym);
+	   //throw parseex_i;
      }
-     throw;
    }
 }
 
 class connruleex : public exception
 {
+	public:
   virtual const char* what () const throw ()
   {
     return "Exception: invalid connrule in definition file";
@@ -306,22 +338,24 @@ try
       smz->getsymbol (cursym);
       if (cursym.get_value() == semicolon) { // cursym is ';'
 	return;
-      } else throw connruleex_i; // we expected a ';'
-    } else throw connruleex_i; // we expected a '.'
-    } else throw connruleex_i; // we expected a '>'
+      } else {errcount++; throw connruleex_i;} // we expected a ';'
+    } else {errcount++; throw connruleex_i;} // we expected a '.'
+    } else {errcount++; throw connruleex_i;} // we expected a '>'
   }
  catch (exception& e)
    {
-     cout << e.what() << endl;
+     //cout << e.what() << endl;
+	smz->print_err(e.what());
      while (!(cursym.get_value() == semicolon)) { // iterates while cursym is not ';'
        smz->getsymbol (cursym);
      }
-     throw;
+    //throw parseex_i;
    }
 }
 
 class devruleex : public exception
 {
+	public:
   virtual const char* what () const throw ()
   {
     return "Exception: invalid devrule in definition file";
@@ -343,26 +377,46 @@ try
 	smz->getsymbol (cursym);
       if (cursym.get_value() == semicolon) { // we have a ';'
 	return;
-      } else throw devruleex_i; // we expected a ';'
-    } else throw devruleex_i; // we expected an '='
+      } else {errcount++; throw devruleex_i;} // we expected a ';'
+    } else {errcount++; throw devruleex_i;} // we expected an '='
   }
  catch (exception& e)
    {
-     cout << e.what() << endl;
+    // cout << e.what() << endl;
+	smz->print_err(e.what());
      while (!(cursym.get_value() == semicolon)) { // iterates while cursym is not ';'
        smz->getsymbol (cursym);
      }
-     throw;
+    //throw parseex_i;
    }
 }
 
 class sectionex : public exception
 {
+	public:
   virtual const char* what () const throw ()
   {
-    return "Exception: invalid section in definition file";
+    return "Exception: invalid Section in definition file";
   }
 } sectionex_i;
+
+class lbrakex : public sectionex
+{
+	public:
+  virtual const char* what () const throw ()
+  {
+    return "Exception: expected '{' at beginning of Section";
+  }
+} lbrakex_i;
+
+class rbrakex : public sectionex
+{
+	public:
+  virtual const char* what () const throw ()
+  {
+    return "Exception: expected '}' at beginning of Section";
+  }
+} rbrakex_i;
 
 void parser::section (void) // throws sectionex if finds invalid section
 {
@@ -374,43 +428,48 @@ try
 	smz->getsymbol (cursym);
       if (cursym.get_value() == lbrak) { // cursym is '{'	
 	smz->getsymbol (cursym);
-	while (cursym.get_value() != rbrak) { // i.e. while inside braces
+	while (cursym.get_value() != rbrak && cursym.get_value() != endfile) { // i.e. while inside braces
 	  devrule();
 	  smz->getsymbol (cursym);
 	}
-      } else throw sectionex_i; // we expected an '{'
+      } else {errcount++; throw lbrakex_i;} // we expected an '{'
 
     } else if (cursym.get_value() == CONNECTIONS) { // cursym is 'CONNECTIONS'
       smz->getsymbol (cursym);
       if (cursym.get_value() == lbrak) { // cursym is '{'
 	smz->getsymbol (cursym);
-	while (!(cursym.get_value() == rbrak)) { // i.e. while inside braces
+	while (cursym.get_value() != rbrak && cursym.get_value() != endfile) { // i.e. while inside braces
 	  connrule();
 	  smz->getsymbol (cursym);
 	}
-      } else throw sectionex_i; // we expected an '{'
+      } else {errcount++; throw lbrakex_i;} // we expected an '{'
 
     } else if (cursym.get_value() == MONITOR) { // cursym is 'MONITOR'
       smz->getsymbol (cursym);
       if (cursym.get_value() == lbrak) { // cursym is '{'
 	smz->getsymbol (cursym);
-	while (!(cursym.get_value() == rbrak)) { // i.e. while inside braces
+	while (cursym.get_value() != rbrak && cursym.get_value() != endfile) { // i.e. while inside braces
 	  monrule();
-	  cout << "monrule ran" << endl;
+	 // cout << "monrule ran" << endl;
 	  smz->getsymbol (cursym);
 	}
-      } else throw sectionex_i; // we expected an '{'
-    } else throw sectionex_i; // didn't get a valid section keyword
+      } else {errcount++; throw lbrakex_i;} // we expected an '{'
+    } else {errcount++; throw sectionex_i;} // didn't get a valid section keyword
   }
-catch (exception& e)
+catch (sectionex& e)
    {
-     cout << e.what() << endl;
-     throw;
+     //cout << e.what() << endl;
+	smz->print_err(e.what());
+	while (!(cursym.get_value() == rbrak)) { // iterates while cursym is not '}'
+       smz->getsymbol (cursym);
+     }
+     //throw parseex_i;
    }
 }
 
 void parser::parsedeffile (void)
 {
+	try {
   do {
 //cout << "start parsedeffile" << endl;
     /*   cout << "type_var: " << cursym.get_type() << endl;
@@ -419,4 +478,9 @@ void parser::parsedeffile (void)
     cout << "uname_var: " << cursym.get_uname() << endl; */
     section();
   } while (!(cursym.get_value() == endfile)); // i.e. while not eof
+}
+catch (exception& e) {
+	cout << "parsedeffileexception: " << e.what() << endl;
+	//throw;
+}
 }
