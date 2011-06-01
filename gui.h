1c73a1741f3c2018515456463b0a536cfbc80ed7
Oliver  Lambert <ocl20@cam.ac.uk>
2011-06-01 21:51:40 +0100

    Added new features to the GUI and made the help work
    Signed-off-by: Oliver  Lambert <ocl20@cam.ac.uk>

--- gui.h
+++ gui.h
@@ -9,27 +9,33 @@
 #include "names.h"
 #include "devices.h"
 #include "monitor.h"
-#include "wx/help.h" // Required for help file
-#include "wx/fs_zip.h"	// Required for help file
-#include <wx/wxhtml.h>	// Required for viewing html
+#include "wx/help.h"			// Required for help file
+#include "wx/fs_zip.h"			// Required for help file
+#include "wx/image.h"			// Required for help file
+#include "wx/html/helpfrm.h"	// Required for help file
+#include "wx/html/helpctrl.h"	// Required for help file
+#include "wx/filesys.h"			// Required for help file
+#include "wx/wxprec.h"			// Required for help file
+#include <wx/wxhtml.h>			// Required for help file
 
-enum { 
+class DeviceConfigDialog;
+class MyGLCanvas;
+
+enum
+{ 
   MY_SPINCNTRL_ID = wxID_HIGHEST + 1,
   MY_TEXTCTRL_ID,
   RUN_BUTTON_ID,
   CONT_BUTTON_ID,
-  SWITCH_BUTTON_ID,
+  DEVICE_BUTTON_ID,
   MONITOR_BUTTON_ID,
 }; // widget identifiers
-
-class DeviceConfigDialog;
-class MyGLCanvas;
 
 class MyFrame: public wxFrame
 {
 public:
 	MyFrame(wxWindow *parent, const wxString& title, const wxPoint& pos, const wxSize& size, 
-		names *names_mod = NULL, devices *devices_mod = NULL, monitor *monitor_mod = NULL, wxHelpController *m_helpController = NULL,
+		names *names_mod = NULL, devices *devices_mod = NULL, monitor *monitor_mod = NULL, /*wxHelpController *m_helpController = NULL,*/
 		long style = wxDEFAULT_FRAME_STYLE); // constructor
 private:
 		// PRIVATE VARIABLES
@@ -38,7 +44,7 @@ private:
 	names *nmz;										// pointer to names class
 	devices *dmz;									// pointer to devices class
 	monitor *mmz;									// pointer to monitor class
-	wxHelpController *hpc;							// pointer to help controller
+	wxHtmlHelpController help;						// Create help controller
 	int cyclescompleted;							// how many simulation cycles have been completed
 		// EVENT HANDLERS
 	void runnetwork(int ncycles);					// function to run the logic network
@@ -46,11 +52,11 @@ private:
 	void OnOpen(wxCommandEvent& event);				// callback for [File | Open] menu item
 	void OnSave(wxCommandEvent& event);				// callback for [File | Save] menu item
 	void OnExit(wxCommandEvent& event);				// callback for [File | Exit] menu item
-	void OnHelpContents(wxCommandEvent& event);				// callback for [Help | View help] menu item
+	void OnHelpContents(wxCommandEvent& event);		// callback for [Help | View help] menu item
 	void OnAbout(wxCommandEvent& event);			// callback for [Help | About] menu item
 	void OnRunButton(wxCommandEvent& event);		// callback for push button (run)
 	void OnContButton(wxCommandEvent& event);		// callback for push button (cont)
-	void OnSwitchButton(wxCommandEvent& event);		// callback for push button (SWITCH)
+	void OnDeviceButton(wxCommandEvent& event);		// callback for push button (DEVICES)
 	void OnMonitorButton(wxCommandEvent& event);	// callback for push button (MONITOR)
 	void OnSpin(wxSpinEvent& event);				// callback for spin control
 	void OnText(wxCommandEvent& event);				// callback for text entry field
@@ -58,29 +64,27 @@ private:
 	DECLARE_EVENT_TABLE()
 };
 
+enum
+{
+	// 
+	ID_SWITCH_1,
+	ID_SWITCH_2,
+	ID_SWITCH_3,
+	
+	ID_APPLY_SETTINGS_TO,
+	ID_BACKGROUND_STYLE,
+	ID_FONT_SIZE
+};
+
 class DeviceConfigDialog: public wxPropertySheetDialog
 {
 //DECLARE_CLASS(DeviceConfigDialog)
 public:
     DeviceConfigDialog(wxWindow* parent);
-
-    wxPanel* CreateSwitchPropertiesPage(wxWindow* parent);
-    //wxPanel* CreateClockPropertiesPage(wxWindow* parent);
-
-protected:
-
-    enum {
-        ID_SHOW_TOOLTIPS = 100,
-        ID_AUTO_SAVE,
-        ID_AUTO_SAVE_MINS,
-        ID_LOAD_LAST_PROJECT,
-
-        ID_APPLY_SETTINGS_TO,
-        ID_BACKGROUND_STYLE,
-        ID_FONT_SIZE
-    };
-
-DECLARE_EVENT_TABLE()
+	wxPanel* CreateSwitchPropertiesPage(wxWindow* parent);
+    wxPanel* CreateClockPropertiesPage(wxWindow* parent);
+private:
+	DECLARE_EVENT_TABLE()
 };
     
 class MyGLCanvas: public wxGLCanvas
--- guitest.cc
+++ guitest.cc
@@ -13,17 +13,12 @@ bool MyApp::OnInit()
 	dmz = new devices(nmz, netz);
 	mmz = new monitor(nmz, netz);
 
-  	// Add Help files
-	wxFileSystem::AddHandler(new wxZipFSHandler);
-	m_helpController = new wxHelpController;
-	m_helpController->Initialize("help.chm");
-
 	// glutInit cannot cope with Unicode command line arguments, so we pass
 	// it some fake ASCII ones instead
 	char *a[2]={"dummy","dummy"};
 	char **tmp1=(char**)a; int tmp2 = 0; glutInit(&tmp2, tmp1);
 	// Construct the GUI
-	MyFrame *frame = new MyFrame(NULL, wxT("Penguin Logic"), wxDefaultPosition,  wxSize(800, 600), nmz, dmz, mmz, m_helpController, wxDEFAULT_FRAME_STYLE);
+	MyFrame *frame = new MyFrame(NULL, wxT("Penguin Logic"), wxDefaultPosition,  wxSize(800, 600), nmz, dmz, mmz, /*m_helpController,*/ wxDEFAULT_FRAME_STYLE);
 	frame->SetBackgroundColour(wxColour(204, 204, 255));
 	frame->Show(true);
 
