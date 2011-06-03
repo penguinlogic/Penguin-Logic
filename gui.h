0fd4cb4ee5c3e0cffc1e13b08ac7064c96bc7b17
Andrew Marshall <ajm268@cam.ac.uk>
2011-06-03 09:30:45 +0100

    Modified gui.h to make it work on Linux.
    Still plenty of compiler errors

--- gui.h
+++ gui.h
@@ -41,7 +41,7 @@ struct switchProp
 	wxStaticText* field_box;
 	wxComboBox* value_box;
 };
-
+static vector<switchProp> DEFAULT_SWITCH;
 // Define clock data structure (for use in device config dialog box)
 struct clockProp
 {
@@ -51,6 +51,7 @@ struct clockProp
 	wxStaticText* field_box;
 	wxSpinCtrl* value_box;
 };
+static vector<clockProp> DEFAULT_CLOCK;
 
 class MyFrame: public wxFrame
 {
@@ -103,8 +104,8 @@ public:
     MyDeviceConfigDialog(wxWindow* parent, wxWindowID id, const wxString& title, const wxPoint& pos,
 						const wxSize& sz, long style, const wxString& name,
 						names *names_mod = NULL, devices *devices_mod = NULL, monitor *monitor_mod = NULL, 
-						network *network_mod = NULL,vector<switchProp> &switchTable = vector<switchProp>(),
-						vector<clockProp> &clockTable = vector<clockProp>());
+						network *network_mod = NULL,vector<switchProp> &switchTable = DEFAULT_SWITCH,
+						vector<clockProp> &clockTable = DEFAULT_CLOCK);
 	wxPanel* CreateSwitchPropertiesPage(wxWindow* parent,vector<switchProp> &switchTable);
     wxPanel* CreateClockPropertiesPage(wxWindow* parent,vector<clockProp> &clockTable);
 private:
